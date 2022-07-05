fa685324804a67ddd1bcf8fa977fc0c25bdccd75
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2022-07-05 12:26:22 -0700

    setup basics

--- src_py/ac.py
+++ src_py/ac.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import time
 # play around with some analytic continuation code
 # goal shape into class structure and interface with TRIQS?
 import numpy as np
@@ -7,7 +7,7 @@ from triqs.gf import *
 import matplotlib.pyplot as plt
 
 # Ref for ADMM algorithm: 10.1103/PhysRevE.95.061302
-# G = K ρ
+# G = K A
 # Gi ≡ G(τi) : vector
 # Kij ≡ K+(τi, ωj)
 # Aⱼ ≡ A(ωⱼ)*Δω
@@ -36,23 +36,20 @@ import matplotlib.pyplot as plt
 #
 
 
-def calc_K_matrix(ωmax, β, M, N, sign="fermion"):
+def calc_K_matrix(ωmin, ωmax, β, shape, sign="fermion"):
+    M, N = shape[0], shape[1]
     # K±(τ,ω) = exp(-τ ω) / ( 1 ± exp(-β ω) )
     # K+(τ,ω) = 1 / ( exp(τω) + exp((-β+τ)*ω) )
     # K-(τ,ω) = 1 / ( exp(τω) - exp((-β+τ)*ω) )
     # + = fermion
     # - = boson
-
     cutoff = 50.0
-
     K = np.zeros((M,N), dtype=float)
 
-    dτ = β/(M-1)
-    dω = 2*ωmax/(N-1)
-    for iω in range(N):
-        ω = iω*dω
-        for iτ in range(M):
-            τ = iτ*dτ
+    ωmesh = np.linspace(ωmin, ωmax, N)
+    τmesh = np.linspace(0, β, M)
+    for iω,ω in enumerate(ωmesh):
+        for iτ,τ in enumerate(τmesh):
             τω = τ*ω
             βmτω = (-β+τ)*ω
             expτω = np.exp(τω) if τω <  cutoff else np.exp(cutoff)
@@ -64,7 +61,7 @@ def calc_K_matrix(ωmax, β, M, N, sign="fermion"):
     return K
 
 
-def calc_svd(K ):
+def calc_svd(K):
     return np.linalg.svd(K)
 
 def update( # figure of arguments
@@ -96,6 +93,37 @@ def setup_mock_Gtau():
 
 if __name__ == "__main__":
     Gtau, Gw = setup_mock_Gtau()
+    beta = 10.0
+    ωmin, ωmax = -10, 10
+    K = calc_K_matrix(ωmin, ωmax, beta, shape=(Gtau.data.shape[0], 1001))
+    M, N = K.shape
+    start = time.time()
+    U, S, V = calc_svd(K)
+    stop = time.time()
+    print("SVD computation time = ", (stop-start), "s")
+    # drop small values
+    L = len(np.where(S>1e-10)[0])
+    print('-'*80)
+    print('singular values: ')
+    for i in range(L): print("S[", i, "] = ", S[i] )
+    print('-'*80)
+
+    # setup  arrays with new dimension L
+    U = U[:,:L]
+    S = S[:L]
+    V = V[:,:L]
+
+    Gout = np.zeros(M)
+    Aout = np.zeros(N)
+    #Aout = estimate(Gqmc, K, U, S, V)
+    #Gout = np.dot(K, Aout)
+
+
+
+
+
+
+    # plot A(ω) = -1/π Im G(ω)
     #plt.figure()
     #plt.plot((-1/np.pi)*Gw.data[:,0,0].imag)
     #plt.show()
